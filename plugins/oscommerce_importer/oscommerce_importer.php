48f94dd6d7790e825794ccd3297b0a813c0bce95
mactive <mactive@gmail.com>
2013-04-27 02:17:17 +0800

    transform goods gallery

--- plugins/oscommerce_importer/oscommerce_importer.php
+++ plugins/oscommerce_importer/oscommerce_importer.php
@@ -41,8 +41,8 @@ function oscimp_getproducts($product_cnt=1) {
 function oscimp_admin() {
 	// include('oscommerce_import_admin.php');
 	// include('oscommerce_import_goods.php');
-	include('oscommerce_update_goods_image.php');	//指定image 然后刷新数量
-	// include('oscommerce_update_goods_gallery.php');	//gallery 机制
+	// include('oscommerce_update_goods_image.php');	//指定image 然后刷新数量
+	include('oscommerce_update_goods_gallery.php');	//gallery 机制
 }
 
 function oscimp_admin_actions() {
