e4e4fcd5c6947b91e8e8f2acf77b091166633166
mactive <mactive@gmail.com>
2013-05-04 22:09:28 +0800

    brand wall

---
+++ plugins/oscommerce_importer/templates/widgets/brand_wall.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Show a brands description when on a taxonomy page
+ */
+?>
+
+
+<?php global $woocommerce; ?>
+<?php if ( $posts ) : ?>
+
+<div id="brand_wall_div">
+    <div class="brand_wall_title">
+        <a href="" class="black_block">推荐厂牌 Top Brand</a>
+        <a href="" class="gray_block">所有品牌 All Brand</a>
+    </div>
+
+	<ul class="brand_wall">
+	<?php 
+		foreach($posts as $post){
+	?>
+		<li style="width: <?php _e($width); ?>px !important;">
+            <a href="brand/<?php _e($post->slug)?>" title="<?php echo $post->name ?>" class="radius_3px">
+                <?php echo $post->name ?>
+            </a>
+        </li>
+	<?php } ?>
+	</ul>
+</div>
+
+
+<?php endif; ?>
+<?php echo wpautop( wptexturize( term_description() ) ); ?>
