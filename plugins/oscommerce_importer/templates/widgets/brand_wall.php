7d3608b3d4afa015f5f7747f8a79be87f9f0680e
mactive <mactive@gmail.com>
2013-05-24 14:46:45 +0800

    brand_list

--- plugins/oscommerce_importer/templates/widgets/brand_wall.php
+++ plugins/oscommerce_importer/templates/widgets/brand_wall.php
@@ -3,7 +3,43 @@
  * Show a brands description when on a taxonomy page
  */
 ?>
+<script type="text/javascript">
+jQuery(document).ready(function ($) {
+    $('ul.brand_wall').width('3000px');
+    var marginLeft = $('ul.brand_wall li').css('marginLeft').replace("px", "");
+    var width =  $('ul.brand_wall li').width()+ marginLeft*2;
+    var count = <?php echo $count; ?>;
+    $('a.brand_left_handle').click(function(){
+        var offset = $('ul.brand_wall').offset();
+        console.log(offset.left);
 
+        if (offset.left > -width * 2) {
+            $('ul.brand_wall').animate({
+                left: '-='+width
+                }, 500, function() {
+                // Animation complete.
+            });
+        };
+    });
+
+    $('a.brand_right_handle').click(function(){
+        var offset = $('ul.brand_wall').offset();
+        console.log(offset.left);
+
+        if (offset.left < width ){
+            $('ul.brand_wall').animate({
+                left: '+='+width
+                }, 500, function() {
+                // Animation complete.
+            });
+        };
+    });
+
+
+
+
+});
+</script>
 
 <?php global $woocommerce; ?>
 <?php if ( $posts ) : ?>
@@ -13,18 +49,31 @@
         <a class="black_block">推荐厂牌 Top Brand</a>
         <a href="brand-list/" class="gray_block">所有品牌 All Brand</a>
     </div>
+    <section class="brand_container">
+        <a class="brand_left_handle block"></a>
+        <div class="brand_center">
+        	<ul class="brand_wall">
+            	<?php 
+            		foreach($posts as $post){
+                    $url = get_brand_thumbnail_url($post->term_id);
+            	?>
 
-	<ul class="brand_wall">
-	<?php 
-		foreach($posts as $post){
-	?>
-		<li style="width: <?php _e($width); ?>px !important;">
-            <a href="brand/<?php _e($post->slug)?>" title="<?php echo $post->name ?>" class="block radius_3px">
-                <?php echo $post->name ?>
-            </a>
-        </li>
-	<?php } ?>
-	</ul>
+            		<li style="width: <?php _e($width); ?>px !important;">
+
+                        <a href="brand/<?php _e($post->slug)?>" 
+                            title="<?php echo $post->name ?>" class="block radius_3px"
+                            style="background-image:url(<?php echo $url; ?>);">
+                        </a>
+                        <span><?php echo $post->name;?></span>
+
+                    </li>
+            	<?php } ?>
+        	</ul>
+        </div>
+        <a class="brand_right_handle block"></a>
+
+    </section>
+
 </div>
 
 
