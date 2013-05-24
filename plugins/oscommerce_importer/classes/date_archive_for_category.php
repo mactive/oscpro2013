363f43af6f7cb355c55263555dfb21f92794cdf3
mactive <mactive@gmail.com>
2013-05-24 11:15:37 +0800

    get 新闻页面也筛选 和 case 大图done

--- plugins/oscommerce_importer/classes/date_archive_for_category.php
+++ plugins/oscommerce_importer/classes/date_archive_for_category.php
@@ -3,22 +3,22 @@ function my_date_archive_for_category() {
 
         $request = $_SERVER['REQUEST_URI'];
 
-	if(strpos($request, "/archives/category/") !== false) {
+        $request_parts = explode('/', $request);
 
-		$request_parts = explode('/', $request);
-
-		// print_r($request_parts);
-
-        /* lets make sure yyyy and mm are integers */
-		$request_parts["4"] = intval($request_parts["4"]);
+        $request_parts["4"] = intval($request_parts["4"]);
 		if($request_parts["4"] < 0) $request_parts["4"] = 0;
 
 		$request_parts["5"] = intval($request_parts["5"]);
 		if($request_parts["5"] < 0) $request_parts["5"] = 0;
 
-		/*
-		lets check if all parts are in place
-		*/
+
+	if(strpos($request, "/archives/category/") !== false &&  $request_parts["4"]> 0 && $request_parts["5"]> 0) {
+
+
+		// print_r($request_parts);
+
+        /* lets make sure yyyy and mm are integers */
+
 
 		if(($request_parts[1] == 'archives') && ($request_parts[2] == 'category') && (!empty($request_parts["3"])) && (!empty($request_parts["4"])) && (!empty($request_parts["5"]))) {
 
@@ -54,48 +54,6 @@ function my_date_archive_for_category() {
 				$is404 = true;
 			}
 		}
-		else {
-			/*
-			 * 正常的分页数据
-			 * /archives/category/cases/page/2
-			 * 
-			 */
-
-			$category_array = get_term_by('slug', $request_parts["3"], "category", ARRAY_A);
-
-			if(!empty($category_array)) {
-				if ($request_parts["4"] == 'page' && !empty($request_parts["5"]) ) {
-					# code...
-					query_posts(array(
-						'cat' => $category_array["term_id"],
-						'paged' => $request_parts["5"]
-					));
-				}else{
-					// $args = array( 'category' => $category_array["term_id"] );
-					// global $posts;
-					// $posts = get_posts( $args );
-					// print_r($posts);
-					// query_posts(array(
-					// 	'cat' => $category_array["term_id"],
-					// ));
-				}
-				
-			}
-
-			$os = array("news", "cases","videoaudio");
-
-			if (in_array($category_array['slug'] , $os)) {
-				# code...
-				echo "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&";
-
-				include(TEMPLATEPATH . '/category-'.$category_array['slug'].'.php');
-
-			}else{
-				include(TEMPLATEPATH . '/category.php');
-			}
-
-
-		}
 
 		if($is404) {
 			include(TEMPLATEPATH . '/404.php');
@@ -105,8 +63,9 @@ function my_date_archive_for_category() {
 	}
 }
 
-// add_action('init','my_date_archive_for_category');
+add_action('init','my_date_archive_for_category');
 // 看看日期的问题如何解决吧
 // http://local.osc-pro.com/archives/category/news/2013/05
+
 
 ?>
