513de5b666aad9c084dc210cfce1680f4c5de91b
mactive <mactive@gmail.com>
2013-06-20 23:01:01 +0800

    图片宽度 展开更多

--- plugins/oscommerce_importer/templates/widgets/catside_date.php
+++ plugins/oscommerce_importer/templates/widgets/catside_date.php
@@ -3,10 +3,28 @@
  * Show a brands description when on a taxonomy page
  */
 ?>
-
 <script type="text/javascript" charset="utf-8" async defer>
     jQuery(document).ready(function ($) {
         $(function() {
+            var count = $("ul.calendar li").length;
+            var default_count = 18;
+            if (count > default_count) {
+              $('a#more').show();
+            }else{
+              $('a#more').hide();
+            }
+
+
+            $("ul.calendar li").each(function(index,Element){
+              if (index > default_count) {
+                $(this).hide();
+              };
+            });
+
+            $('a#more').click(function(){
+              $("ul.calendar li").show();
+              $(this).hide();
+            });
             //
             $('.calendar a')
                .each(function()
@@ -24,3 +42,5 @@
         <ul class="calendar">
             <?php wp_get_archives($args) ?>
         </ul>
+
+        <a id="more" class="calendar_more">展开更多&darr;</a>
