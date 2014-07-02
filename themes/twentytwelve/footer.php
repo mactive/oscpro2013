bfd2cf9b44c14bc3cd5313bbb4aea7ea21d0cf93
mactive <mactive@gmail.com>
2014-07-02 18:28:02 +0800

    remove google temp

--- themes/twentytwelve/footer.php
+++ themes/twentytwelve/footer.php
@@ -57,13 +57,13 @@
 </div>
 <?php wp_footer(); ?>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-5684234-2', 'osc-pro.com');
-  ga('send', 'pageview');
+  // ga('create', 'UA-5684234-2', 'osc-pro.com');
+  // ga('send', 'pageview');
 
 </script>
 </body>
